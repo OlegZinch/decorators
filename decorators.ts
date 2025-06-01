8c132a3ec2bcbd9da7857e190a32ee45e0247dfb
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-06-01 14:06:22 +0100

    added method decorator

--- decorators.ts
+++ decorators.ts
@@ -1,5 +1,24 @@
-function logger(target: any, ctx: ClassDecoratorContext) {
+function logger<T extends new (...arg: any[]) => any>(
+  target: T,
+  ctx: ClassDecoratorContext
+) {
   console.log('logger decorator')
+  console.log(target)
+  console.log(ctx)
+
+  return class extends target {
+    constructor(...arg: any[]) {
+      super(...arg)
+      console.log('class constructor')
+      console.log(this)
+    }
+  }
+}
+
+function autobind(
+  target: (...args: any[]) => any,
+  ctx: ClassMethodDecoratorContext
+) {
   console.log(target)
   console.log(ctx)
 }
@@ -8,7 +27,11 @@ function logger(target: any, ctx: ClassDecoratorContext) {
 class Person {
   name = 'Oleh'
 
+  @autobind
   greet() {
     console.log('Hi, I am ' + this.name)
   }
 }
+
+const oleh = new Person()
+const julie = new Person()
